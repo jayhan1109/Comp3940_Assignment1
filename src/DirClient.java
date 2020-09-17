df26f01b21607d3a5710f2a8d9d360c7c0a48ed9
jayhan1109 <jayhan1109@gmail.com>
2020-09-17 22:34:10 +0900

    User Agent / Path parsing

--- src/DirClient.java
+++ src/DirClient.java
@@ -3,13 +3,15 @@ import java.net.*;
 
 public class DirClient {
     public static void main(String[] args) {
-        String result="";
+        String result = "";
         try (Socket socket = new Socket("localhost", 8086);
              PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
         ) {
             String fromServer;
-            out.println(args[0]);
+            out.println("GET " + args[0] + " HTTP/1.0\r\n");
+            out.println("User-Agent: Console");
+
             while ((fromServer = in.readLine()) != null) {
                 result += fromServer + "\n";
             }
--- src/DirServerThread.java
+++ src/DirServerThread.java
@@ -15,10 +15,32 @@ public class DirServerThread extends Thread {
              BufferedReader in = new BufferedReader(new InputStreamReader(
                      socket.getInputStream()));
         ) {
+
+            String inputLine, outputLine;
+            String request = "";
+            String path = "";
+            String userAgent = "";
+            while ((inputLine = in.readLine()) != null) {
+                request += inputLine + "\n";
+                if (path.equals("") && request.contains("GET")) {
+                    path = getPath(request);
+                    System.out.println(path);
+                }
+
+                if (request.contains("User-Agent")) {
+                    userAgent = getUserAgent(request);
+                    System.out.println(userAgent);
+
+                    isConsole = userAgent.equals("Console");
+                    System.out.println(isConsole);
+
+                    break;
+                }
+            }
+
+
             if (isConsole) {
-                String inputLine, outputLine;
-                inputLine = in.readLine();
-                File dir = new File(inputLine);
+                File dir = new File(path);
                 String[] chld = dir.list();
                 if (chld == null) {
                     out.println("Invalid Directory\n");
@@ -28,22 +50,37 @@ public class DirServerThread extends Thread {
                         out.println(fileName + "\n");
                     }
                 }
-            } else {
-                System.out.println(in.readLine());
-               
-                out.write("HTTP/1.0 200 OK\n");
-                out.flush();
-                out.write("Content-Type: text/html\n\n");
-                out.flush();
-
-                out.write("<h1>Welcome!.</h1>");
-                out.flush();
             }
+//            else {
+//                System.out.println(in.readLine());
+//
+//                out.write("HTTP/1.0 200 OK\n");
+//                out.flush();
+//                out.write("Content-Type: text/html\n\n");
+//                out.flush();
+//
+//                out.write("<h1>Welcome!.</h1>");
+//                out.flush();
+//            }
 
             socket.close();
         } catch (Exception e) {
             System.out.println("Exception in thread: " + Thread.currentThread().getId() + "\nMessage: " + e.getMessage() + "\n");
         }
     }
+
+    String getPath(String request) {
+        int start = request.indexOf("GET ") + 4;
+        int end = request.indexOf(" HTTP/");
+        request = request.substring(start, end);
+        return request;
+    }
+
+    String getUserAgent(String request) {
+        int start = request.indexOf("User-Agent: ") + 12;
+        request = request.substring(start,start+7);
+        return request;
+    }
+
 }
 
